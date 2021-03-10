f757c645e288e015d46455022d6dcb91a352bf0d
Fenil Mehta <fenilgmehta@gmail.com>
2021-03-10 21:21:34 +0530

    Bug fixes, New features, More debug & info prints, Fix load generation
    - Moved run-time client configuration code to client_common_services.py
    - Fix signal_handler to close and delete FIFO file used for run-time configuration

--- client.py
+++ client.py
@@ -1,16 +1,16 @@
-import signal
-import struct
-import os
+import fcntl
 import json
 import logging
-import sys
+import os
+import random
+import signal
 import socket
-import fcntl
+import struct
 import sys
 import time
-import random
-import threading
-import pprint
+from typing import Dict
+
+from client_common_services import ClientFIFO
 
 
 def make_fd_non_blocking(file_fd):
@@ -20,100 +20,6 @@ def make_fd_non_blocking(file_fd):
     # REFER: https://stackoverflow.com/questions/375427/a-non-blocking-read-on-a-subprocess-pipe-in-python
     fl = fcntl.fcntl(file_fd, fcntl.F_GETFL)
     fcntl.fcntl(file_fd, fcntl.F_SETFL, fl | os.O_NONBLOCK)
-
-
-def handle_input(user_input, client_conf):
-    user_input = user_input.strip()
-    if len(user_input) == 0:
-        return
-    logging.debug(f'len(user_input) = {len(user_input)}')
-    logging.debug(f'user_input = "{user_input}"')
-    input_parts = user_input.split()
-    if input_parts[0] == '-':
-        # remove the server from list
-        try:
-            logging.debug(client_conf['server_address_port'])
-            logging.debug(input_parts)
-            logging.debug([(input_parts[1], int(input_parts[2]),)])
-            client_conf['server_address_port'].remove(
-                (input_parts[1], int(input_parts[2]),)
-            )
-        except ValueError:
-            logging.debug('Removal of server failed')
-    elif input_parts[0] == '+':
-        # add the server to the list
-        client_conf['server_address_port'].append((input_parts[1], int(input_parts[2])))
-    elif input_parts[0] == 'low':
-        client_conf['req_load'] = 'low'
-    elif input_parts[0] == 'high':
-        client_conf['req_load'] = 'high'
-    else:
-        logging.warning(f'Incorrect user_input = "{user_input}"')
-    return
-
-
-def start_sending_requests(client_conf, message_conf) -> None:
-    logging.debug('start_sending_requests(...) STARTED :)')
-    client_conf['server_address_port'] = [(client_conf['server_ip'], int(client_conf['server_port']))]
-
-    # Create a UDP socket
-    udp_client_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-
-    # NOTE: bind is optional for client
-
-    # https://stackoverflow.com/questions/25426447/creating-non-blocking-socket-in-python
-    udp_client_socket.setblocking(False)
-
-    while True:
-        # Check if there is any change in the list of servers IP and port list or not
-        try:
-            user_input = client_conf['fifo_communication_file_obj'].readline()
-            logging.debug(f'user_input = "{user_input}"')
-            handle_input(user_input, client_conf)
-        except Exception as e:
-            logging.critical(f'Inside "except" clause for "user_input", type(e)={type(e)}, str(e)={str(e)}')
-
-        if len(client_conf['server_address_port']) == 0:
-            logging.debug('Total servers = len(client_conf[\'server_address_port\']) = 0')
-        for server_i in client_conf['server_address_port']:
-            # Generate Random query
-            req_int = random.randint(client_conf['req_num_low'], client_conf['req_num_high'])
-
-            # Format: big-endian and 4 bytes (unsigned int)
-            req_bytes_to_send = struct.pack(">I", req_int)  # max value can be "2**32 - 1"
-            logging.debug(f'Query:')
-            logging.debug(f'    req_int           = {req_int}')
-            logging.debug(f'    req_bytes_to_send = {req_bytes_to_send}')
-
-            # Send to server using created UDP socket
-            udp_client_socket.sendto(req_bytes_to_send, server_i)
-
-        # Receive all server replies without blocking
-        # Format: big-endian and 8 bytes (unsigned long long)
-        try:
-            while True:
-                req_reply_bytes, server_addr = udp_client_socket.recvfrom(8)
-                logging.debug(f'Response of server_addr = {server_addr}')
-                logging.debug(f'    req_reply_bytes = {req_reply_bytes}')
-                req_reply_int = int.from_bytes(req_reply_bytes, 'big')
-                logging.debug(f'    req_reply_int   = {req_reply_int}')
-        except BlockingIOError:
-            pass  # nothing to receive
-        except Exception as e:
-            logging.debug(f'type(e) = {type(e)}')
-            logging.debug(f'e = {e}')
-
-        if client_conf['req_load'] == 'low':
-            time.sleep(1.3)
-            pass
-        elif client_conf['req_load'] == 'high':
-            time.sleep(0.7)
-            pass
-        else:
-            logging.critical('Wrong value stored in "client_conf[\'req_load\']"')
-    # noinspection PyUnreachableCode
-    logging.debug('start_sending_requests(...) ENDED - this will never execute')
-    pass
 
 
 def client_init(client_conf: dict):
@@ -135,11 +41,14 @@ def client_init(client_conf: dict):
             #     os.open(client_conf['fifo_communication_file'], os.O_RDONLY | os.O_NONBLOCK),
             #     'rt'
             # )
+
+            # REFER: https://stackoverflow.com/questions/46537450/python-open-fifo-blocks-forever/46539607
             client_conf['fifo_communication_file_obj'] = open(client_conf['fifo_communication_file'])
             logging.info('FIFO file successfully opened :)')
         except OSError as e:
             logging.warning(f'Failed to create FIFO: {e}')
             problem_with_fifo = True
+            sys.exit(1)
 
     if problem_with_fifo or client_conf['fifo_communication_file'] == '/dev/stdin':
         client_conf['fifo_communication_file'] = '/dev/stdin'
@@ -152,18 +61,112 @@ def client_init(client_conf: dict):
     return
 
 
-GLOBAL_CLIENT_CONF = None
+def start_sending_requests(client_conf, message_conf) -> None:
+    logging.debug('start_sending_requests(...) STARTED :)')
+    client_conf['server_address_port'] = list()
+    if client_conf['server_ip'].lower() != 'none':
+        client_conf['server_address_port'].append(
+            (client_conf['server_ip'], client_conf['server_port'])
+        )
+
+    # Create a UDP socket
+    udp_client_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+    # NOTE: bind is optional for client
+
+    # https://stackoverflow.com/questions/25426447/creating-non-blocking-socket-in-python
+    udp_client_socket.setblocking(False)
+    client_fifo: ClientFIFO = ClientFIFO(client_conf['fifo_communication_file_obj'], 21001)
+
+    server_to_send_req_idx = -1
+    total_requests_sent = 0
+    total_replies_received = 0
+    # Used for the calculation of: average throughput or latency of request processing at the servers
+    avg_throughtput_req_count_X = 50
+    n_minus_X_req_time = time.time_ns()
+    while True:
+        if client_conf['req_load'] not in ('low', 'mid', 'high', 'custom'):
+            logging.critical('Wrong value stored in "client_conf[\'req_load\']"')
+        next_sleep_time = time.time() + client_conf[f'load_request_time_period_seconds_{client_conf["req_load"]}']
+
+        # Check if there is any change in the list of servers IP and port list or not
+        client_fifo.check_fifo(client_conf)
+
+        if len(client_conf['server_address_port']) == 0:
+            logging.debug('Total servers = len(client_conf[\'server_address_port\']) = 0')
+        else:
+            if len(client_conf['server_address_port']) != 0:
+                server_to_send_req_idx = (server_to_send_req_idx + 1) % len(client_conf['server_address_port'])
+
+            # for server_i in client_conf['server_address_port']:
+            # Generate Random query
+            req_int = random.randint(client_conf['req_num_low'], client_conf['req_num_high'])
+
+            # Format: big-endian and 4 bytes (unsigned int)
+            req_bytes_to_send = struct.pack(">I", req_int)  # max value can be "2**32 - 1"
+            logging.debug(f'Query:')
+            logging.debug(f'    req_int           = {req_int}')
+            logging.debug(f'    req_bytes_to_send = {req_bytes_to_send}')
+
+            # Send to server using created UDP socket
+            logging.debug(f"client_conf['server_address_port'][server_to_send_req_idx] = "
+                          f"{client_conf['server_address_port'][server_to_send_req_idx]}")
+            udp_client_socket.sendto(req_bytes_to_send, client_conf['server_address_port'][server_to_send_req_idx])
+            total_requests_sent += 1
+
+        # Receive all server replies without blocking
+        # Format: big-endian and 8 bytes (unsigned long long)
+        try:
+            while True:
+                req_reply_bytes, server_addr = udp_client_socket.recvfrom(8)
+                logging.debug(f'Response of server_addr = {server_addr}')
+                logging.debug(f'    req_reply_bytes = {req_reply_bytes}')
+                req_reply_int = int.from_bytes(req_reply_bytes, 'big')
+                logging.debug(f'    req_reply_int   = {req_reply_int}')
+                total_replies_received += 1
+                if total_replies_received % avg_throughtput_req_count_X == 0:
+                    logging.info(f'Server Request THROUGHPUT = '
+                                 f'{(avg_throughtput_req_count_X * 1000000000) / (time.time_ns() - n_minus_X_req_time):.9f} / second')
+                    n_minus_X_req_time = time.time_ns()
+        # 1 / (((time.time_ns() - n_minus_X_req_time) / 1000000000) / avg_throughtput_req_count_X)
+        except BlockingIOError:
+            pass  # nothing to receive
+        except Exception as e:
+            logging.debug(f'type(e) = {type(e)}')
+            logging.debug(f'e = {e}')
+
+        logging.info(f'STATS: total_requests_sent = {total_requests_sent}')
+        logging.info(f'STATS: total_replies_received = {total_replies_received}')
+        logging.info(f'STATS: PENDING replies = {total_requests_sent - total_replies_received}')
+        logging.info(f"STATS: client_conf['server_address_port'] = {client_conf['server_address_port']}")
+        if time.time() < next_sleep_time:
+            time.sleep(next_sleep_time - time.time())
+    # noinspection PyUnreachableCode
+    logging.debug('start_sending_requests(...) ENDED - this will never execute')
+    pass
+
+
+GLOBAL_CLIENT_CONF: Dict = dict()
 
 
 def signal_handler(sig, frame):
     # REFER: https://stackoverflow.com/questions/1112343/how-do-i-capture-sigint-in-python
-    print('\n\nYou pressed Ctrl+C !\n')
-    GLOBAL_CLIENT_CONF['fifo_communication_file_obj'].close()
+    print('\n\nYou pressed Ctrl+C ! Exiting...\n')
+    if 'fifo_communication_file_obj' in GLOBAL_CLIENT_CONF:
+        GLOBAL_CLIENT_CONF['fifo_communication_file_obj'].close()
     if GLOBAL_CLIENT_CONF['fifo_communication_file'] != '/dev/stdin':
         # https://www.w3schools.com/python/python_file_remove.asp
         os.remove(GLOBAL_CLIENT_CONF['fifo_communication_file'])
+        if os.path.exists(GLOBAL_CLIENT_CONF['fifo_communication_file']):
+            logging.critical(
+                'Due to unknown reason, FIFO file was not deleted with "os.remove(...)". Now using "rm -f ..."'
+            )
+            os.system(f'rm -f "{GLOBAL_CLIENT_CONF["fifo_communication_file"]}"')
     logging.debug(f'Successfully deleted "{GLOBAL_CLIENT_CONF["fifo_communication_file"]}"')
-    pprint.pprint(GLOBAL_CLIENT_CONF)
+    logging.debug("")
+    logging.debug("")
+    # logging.debug(pprint.pformat(GLOBAL_CLIENT_CONF, indent=4))
+    # pprint.pformat(GLOBAL_CLIENT_CONF)
     sys.exit(0)
 
 
@@ -175,9 +178,10 @@ if __name__ == '__main__':
         MESSAGE_CONF = json.load(f)
     with open('client.conf') as f:
         CLIENT_CONF = json.load(f)
+        # CLIENT_CONF = eval(f.read())
 
     # set settings
+    GLOBAL_CLIENT_CONF: Dict = CLIENT_CONF
     client_init(CLIENT_CONF)
-    GLOBAL_CLIENT_CONF = CLIENT_CONF
 
     start_sending_requests(CLIENT_CONF, MESSAGE_CONF)
